b07a38b579dc3f81a3c9a3597efbca86cc04fd3f
John Rozmaryn <jrozmaryn@beckman.com>
2019-09-24 16:54:21 -0400

    added servo testing program

---
+++ armtest/armtest.ino
@@ -0,0 +1,74 @@
+#include <Servo.h>
+Servo lidservo;
+Servo armservo; 
+Servo flag;
+
+int armOff = 177;  //This is the arm value where the switch is turmed off
+int armFake = 155; //This is the arm value that may be touching the switch, but not turning it off 
+int armIn = 50;    //This is the arm value where the arm is inside the box / neutral
+
+int lidClose = 90;  //This is the lid value where the lid is closed
+int lidNarrow = 50;   //This is the lid value where the lid is opened part way
+int lidOpen = 20;   //This is the lid value where the lid is opened all the way
+
+int flagOff = 50;  //This is when the surrender flag is fully down
+int flagStart = 125;   //This is when the surrender flag starts to wave
+int flagEnd = 175;    //This is when the surrender flag ends the wave
+
+int armpin = 7;
+int lidpin = 8
+
+int action = 1;
+//int pos = 0;  //This is from the original code. I don't think it needs to be here.
+
+void setup() {
+pinMode(armpin,INPUT);
+pinMode(lidpin,INPUT);
+lidservo.attach(1);
+armservo.attach(2);
+flag.attach(4);
+
+// Servo Default Starting Position
+
+armservo.write(armIn);
+flag.write(flagOff);
+lidservo.write(lidClose);
+}
+
+void loop() {
+
+// Action Selection starting with 1
+
+  if (digitalRead(armOff) == HIGH)
+	armservo.write(armIn);
+  else
+	armservo.write(armClose);
+ 
+
+  if (digitalRead(lidpin) == HIGH)
+	lidservo.write(lidOpen);
+  else 
+	lidservo.write(lidClose);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
