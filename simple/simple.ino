adf358ea8cab0ffbb2a54f28b7778d7d82846583
johnrozmaryn <john.rozmaryn@gmail.com>
2019-09-24 20:20:39 -0400

    added simple program, playing with pulldown resistors

---
+++ simple/simple.ino
@@ -0,0 +1,67 @@
+#include <Servo.h>
+Servo lidservo;
+Servo armservo; 
+Servo flag;
+
+int armOff = 160;  //This is the arm value where the switch is turmed off
+int armFake = 140; //This is the arm value that may be touching the switch, but not turning it off 
+int armIn = 35;    //This is the arm value where the arm is inside the box / neutral
+
+int lidClose = 90;  //This is the lid value where the lid is closed
+int lidNarrow = 50;   //This is the lid value where the lid is opened part way
+int lidOpen = 20;   //This is the lid value where the lid is opened all the way
+
+int flagOff = 50;  //This is when the surrender flag is fully down
+int flagStart = 125;   //This is when the surrender flag starts to wave
+int flagEnd = 175;    //This is when the surrender flag ends the wave
+
+int switchpin 7;
+
+void setup() {
+    pinMode(switchpin,INPUT_PULLUP);
+    lidservo.attach(2);
+    armservo.attach(5);
+
+// Servo Default Starting Position
+
+    armservo.write(armIn);
+    flag.write(flagOff);
+    lidservo.write(lidClose);
+}
+
+void loop() {
+
+  if (digitalRead(armpin) == LOW) {
+    lidservo.write(lidOpen);
+    delay(1000);
+    armservo.write(armOff);
+    delay(500); 
+    armservo.write(armIn);
+    delay(1000);
+    lidservo.write(lidClose);
+    delay(1000);
+	}
+
+delay(1000);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
